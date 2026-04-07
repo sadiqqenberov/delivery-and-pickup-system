9a720c3ab6d33c11cae262d236835459da0b4dc5
sadiqqenberov <sadiqqenberov26@gmail.com>
2026-04-07 11:12:48 +0400

    Initial commit

---
+++ src/main/java/delivery_and_pickup_system/delivery_and_pickup_system/DeliveryAndPickupSystemApplication.java
@@ -0,0 +1,13 @@
+package delivery_and_pickup_system.delivery_and_pickup_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAndPickupSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAndPickupSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/delivery_and_pickup_system/delivery_and_pickup_system/DeliveryAndPickupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package delivery_and_pickup_system.delivery_and_pickup_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAndPickupSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
